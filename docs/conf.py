4f91db1367f7eb9539b40227c1fe9d9c39500815
Lars Heuer <heuer@semagia.com>
2019-06-30 00:04:20 +0200

    Fixed Sphinx LaTeX output

--- docs/conf.py
+++ docs/conf.py
@@ -196,7 +196,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'pyqrcodeng.tex', 'PyQRCodeNG Documentation',
-   'manual'),
+   'Author', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of

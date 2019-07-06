2d63770708432570774c37bb21866935091cd68a
Lars Heuer <heuer@semagia.com>
2019-07-07 00:32:07 +0200

    Removed Py 2.6 from supported environments

--- setup.py
+++ setup.py
@@ -49,8 +49,8 @@ setup(
     packages=['pyqrcodeng'],
     version=version,
     long_description=read('README.rst', 'CHANGES.rst'),
-    description='Fork of PyQRCode - A QR code generator written purely in Python with SVG, EPS, PNG and terminal output.',
-    author='Michael Nooner, Lars Heuer',
+    description='PyQRCode New Generation -- A QR code generator written purely in Python with SVG, EPS, PNG and terminal output.',
+    author='Lars Heuer',
     author_email='heuer@semagia.com',
     url='https://github.com/pyqrcode/pyqrcodeNG',
     keywords=['qrcode', 'qr'],
@@ -70,7 +70,6 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         ],
 )

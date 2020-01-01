27c3367f2e30eb8afcad1834d692e6d3b5d47827
Lars <heuer@semagia.com>
2020-01-01 12:20:18 +0100

    More test cases for issue #15

--- tests/test_issue_ng15.py
+++ tests/test_issue_ng15.py
@@ -13,12 +13,25 @@ def test_version_too_small():
         pyqrcode.create('A' * 26, version=1, error='l')
 
 
+def test_numeric_defaults():
+    qr = pyqrcode.create('1' * 17)  # Capacity of a 1-H (numeric): 17
+    assert '1-H' == qr.designator
+    assert 'numeric' == qr.mode
+
+
+def test_numeric_explicit_error():
+    qr = pyqrcode.create('1' * 41, error='l')  # Capacity of a 1-L (numeric): 41
+    assert '1-L' == qr.designator
+    assert 'numeric' == qr.mode
+
+
 def test_version_and_error_provided():
     # QR Code version 1-L: Max. 25 alphanumeric chars
     qr = pyqrcode.create('A' * 25, version=1, error='l')
     assert 1 == qr.version
     assert 'L' == qr.error
     assert '1-L' == qr.designator
+    assert 'alphanumeric' == qr.mode
 
 
 if __name__ == '__main__':
